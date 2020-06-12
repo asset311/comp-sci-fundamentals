ff9e782ff52ed1e6644188ea66f859ec5013b1d9
asset311 <32253546+Aseka311@users.noreply.github.com>
2020-06-12 14:46:44 -0400

    binary tree node

---
+++ data_structures/binary_tree_node.py
@@ -0,0 +1,12 @@
+class BinaryTreeNode:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+    
+    def left(self):
+        return self.left
+    
+    def right(self):
+        return self.right
+    
